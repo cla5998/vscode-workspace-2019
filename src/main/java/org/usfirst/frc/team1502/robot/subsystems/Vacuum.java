8b22467a00a1745e8db40383be460405c4e4f2e6
Alafita <1alafiti@csd.local>
2019-01-19 11:02:29 -0500

    Removed unneeded thing

--- src/main/java/org/usfirst/frc/team1502/robot/subsystems/Vacuum.java
+++ src/main/java/org/usfirst/frc/team1502/robot/subsystems/Vacuum.java
@@ -10,7 +10,6 @@ package org.usfirst.frc.team1502.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
