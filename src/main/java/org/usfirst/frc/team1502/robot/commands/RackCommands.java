ef0a1232186c4e19c0c1c222ae8b108e1d0a1dd6
Ackley <0ackleyt@csd.local>
2019-01-19 13:36:14 -0500

    added end phase to rack

--- src/main/java/org/usfirst/frc/team1502/robot/commands/RackCommands.java
+++ src/main/java/org/usfirst/frc/team1502/robot/commands/RackCommands.java
@@ -46,11 +46,13 @@ public class RackCommands extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.rack.setSpeed(0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
