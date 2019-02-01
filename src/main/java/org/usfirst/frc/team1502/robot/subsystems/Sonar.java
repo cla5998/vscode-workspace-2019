0432fbc76a5fefff3e7dcd3ab906d2e7be9be85e
cla5998 <44716425+cla5998@users.noreply.github.com>
2019-01-31 23:17:35 -0500

    added possibility for immutable java maps to be used in sonar

--- src/main/java/org/usfirst/frc/team1502/robot/subsystems/Sonar.java
+++ src/main/java/org/usfirst/frc/team1502/robot/subsystems/Sonar.java
@@ -7,7 +7,9 @@
 
 package org.usfirst.frc.team1502.robot.subsystems;
 
+import java.util.Collections;
 import java.util.EnumMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import edu.wpi.first.wpilibj.AnalogInput;
@@ -24,16 +26,6 @@ public class Sonar extends Subsystem {
     high, low;
   }; // these are the boundaries for the sonar to detect
 
-  private static final double LINEAR_SLIDE_HIGH = 0;
-  private static final double LINEAR_SLIDE_LOW = 0;
-  private static final double PLATFORM_HIGH = 5;
-  private static final double PLATFORM_LOW = 10;; // this is the way to differentiate between what the boundaries
-                                                      // are for. might be unneeded, but its here
-  // private static final double LINEAR_SLIDE_HIGH = 0;
-  // private static final double LINEAR_SLIDE_LOW = 0;
-  // private static final double PLATFORM_HIGH = 0;
-  // private static final double PLATFORM_LOW = 0;;
-
   AnalogInput analogSonar;
 
   double analogVolts; // raw output
@@ -41,26 +33,20 @@ public class Sonar extends Subsystem {
 
   public Sonar(AnalogInput analogSonar) {
     this.analogSonar = analogSonar;
-
-    PlatForm.put(Boundaries.high, 0.0);
-    PlatForm.put(Boundaries.low, 1.0);
-    Rocket.put(Boundaries.high, 0.0);
-    Rocket.put(Boundaries.low, 1.0);
   }
 
-  public static Map<Boundaries, Double> PlatForm = new EnumMap<Boundaries, Double>(Boundaries.class) {
-    { // "error" is because i havent set a serial number for it
-      put(Boundaries.high, 0.0);
-      put(Boundaries.low, 1.0);
-    }
-  };
+  public static Map<Boundaries, Double> PlatForm = new EnumMap<Boundaries, Double>(Boundaries.class) {{ // "error" is because i havent set a serial number for it
+    put(Boundaries.high, 0.0);
+    put(Boundaries.low, 1.0);
+  }};
 
-  public static Map<Boundaries, Double> Rocket = new EnumMap<Boundaries, Double>(Boundaries.class) {
-    {
-      put(Boundaries.high, 0.0);
-      put(Boundaries.low, 1.0);
-    }
-  };
+  public static Map<Boundaries, Double> Rocket = new EnumMap<Boundaries, Double>(Boundaries.class) {{
+    put(Boundaries.high, 0.0);
+    put(Boundaries.low, 1.0);
+  }};
+  
+  Map<Boundaries, Double> PlatFormRef = Collections.unmodifiableMap(new LinkedHashMap<Boundaries, Double>(PlatForm));
+  Map<Boundaries, Double> RocketRef = Collections.unmodifiableMap(new LinkedHashMap<Boundaries, Double>(Rocket));
 
   public double getBound(Map<Boundaries, Double> type, Boundaries distance) { // simple get function. its slick
     return (double) type.get(distance);
