22527e401af4cfc0828966a972fb3590bcf26fa1
cla5998 <44716425+cla5998@users.noreply.github.com>
2019-02-09 01:05:42 -0500

    figured out how to get around the error cad gave me. redid the led file

--- src/main/java/org/usfirst/frc/team1502/robot/subsystems/Led.java
+++ src/main/java/org/usfirst/frc/team1502/robot/subsystems/Led.java
@@ -1,4 +1,4 @@
-*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 /* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
@@ -6,9 +6,6 @@
 /*----------------------------------------------------------------------------*/
 
 package org.usfirst.frc.team1502.robot.subsystems;
-
-import java.util.EnumMap;
-import java.util.Map;
 
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -21,28 +18,39 @@ public class Led extends Subsystem {
   // here. Call these from Commands.
   
   Spark LEDSpark;
-
   public enum Color {
-    Red, Green, Fire
+    Red(.61), Green(.77);
+    private double value;
+
+    Color(double value) {
+      this.value = value;
+    }
+
+    public double get() {
+      return value;
+    }
   };
+
   Color current = Color.Red;
 
-  Map<Color, Double> ledMap = new EnumMap<Color, Double> (Color.class){{
-    put(Color.Red, .61);
-    put(Color.Green, .77);
-    put(Color.Fire, -.39);
-  }};
+  // Map<Color, Double> ledMap = new EnumMap<Color, Double> (Color.class){{
+  //   put(Color.Red, .61);
+  //   put(Color.Green, .77);
+  // }};
 
   public Led(Spark LEDSpark) {
     this.LEDSpark = LEDSpark;
   }
 
-  public void setColor(Color target) {
-    LEDSpark.set(ledMap.get(target));
+  public void setRed() {
+    LEDSpark.set(Color.Red.get());
+  }
+  public void setGreen() {
+    LEDSpark.set(Color.Green.get());
   }
 
   public void colorChange() {
-    current = current == Color.Red ? Color.Green : Color.Fire;
+    current = current == Color.Red ? Color.Green : Color.Red;
   }
 
   @Override
