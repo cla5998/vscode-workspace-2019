7e103bd2121a158ff8608d6064faac0ac4a3c253
Cade <cade@YogaND-MacBook-Pro.local>
2019-02-09 14:54:54 -0500

    black

--- src/main/java/org/usfirst/frc/team1502/robot/subsystems/Led.java
+++ src/main/java/org/usfirst/frc/team1502/robot/subsystems/Led.java
@@ -20,7 +20,7 @@ public class Led extends Subsystem {
   Spark LEDSpark;
 
   public enum Color {
-    Red(.35), Green(.77);
+    Red(.99), Green(.77);
     private double value;
 
     Color(double value) {
